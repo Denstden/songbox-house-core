b4ddb83e746f19bd3f378cbc0f268d11d1849e85
denstden <denstden@gmail.com>
2019-07-28 15:41:34 +0300

    Implemented base core functionality

---
+++ src/main/java/songbox/house/service/UserService.java
@@ -0,0 +1,33 @@
+package songbox.house.service;
+
+import songbox.house.domain.dto.request.UserDto;
+import songbox.house.domain.entity.MusicCollection;
+import songbox.house.domain.entity.user.UserInfo;
+
+import java.util.Set;
+
+public interface UserService {
+
+    UserInfo findByUserName(final String userName);
+
+    UserInfo getCurrentUser();
+
+    String getCurrentUserName();
+
+    UserInfo createAdminIfNotExists();
+
+    UserInfo createUser(final UserDto userDto);
+
+    default boolean checkCanGet(final Set<MusicCollection> collections) {
+        if (collections != null) {
+            for (final MusicCollection collection : collections) {
+                final UserInfo userInfo = collection.getOwner();
+                if (userInfo.getUserName().equals(getCurrentUserName())) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
