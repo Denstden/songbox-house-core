4c2508b7dea2754446b137d38949f9832a35df77
denstden <denstden@gmail.com>
2020-04-11 15:45:43 +0300

    Dev: parallel search

--- src/main/java/songbox/house/service/search/impl/SearchServiceFacadeImpl.java
+++ src/main/java/songbox/house/service/search/impl/SearchServiceFacadeImpl.java
@@ -3,6 +3,7 @@ package songbox.house.service.search.impl;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import songbox.house.domain.dto.request.SearchQueryDto;
 import songbox.house.domain.dto.response.SearchResultDto;
@@ -11,12 +12,15 @@ import songbox.house.service.DiscogsWebsiteService;
 import songbox.house.service.search.SearchService;
 import songbox.house.service.search.SearchServiceFacade;
 import songbox.house.util.ArtistsTitle;
+import songbox.house.util.ExecutorUtil;
 import songbox.house.util.compare.TrackMetadataComparator;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Lists.reverse;
@@ -33,11 +37,14 @@ public class SearchServiceFacadeImpl implements SearchServiceFacade {
 
     List<SearchService> searchServices;
     DiscogsWebsiteService discogsWebsiteService;
+    ExecutorService searchExecutorService;
 
     @Autowired
-    public SearchServiceFacadeImpl(List<SearchService> searchServices, DiscogsWebsiteService discogsWebsiteService) {
+    public SearchServiceFacadeImpl(List<SearchService> searchServices, DiscogsWebsiteService discogsWebsiteService,
+            @Value("${songbox.house.search.threads:2}") Integer searchThreads) {
         this.searchServices = searchServices;
         this.discogsWebsiteService = discogsWebsiteService;
+        this.searchExecutorService = ExecutorUtil.createExecutorService(searchThreads);
     }
 
     @Override
@@ -49,13 +56,11 @@ public class SearchServiceFacadeImpl implements SearchServiceFacade {
         log.info("Starting search for {}", query);
         long searchStart = currentTimeMillis();
 
-
-        final List<TrackMetadataDto> songs = newArrayList();
         CompletableFuture<Optional<String>> artworkFuture = null;
         if (query.isFetchArtwork()) {
             artworkFuture = supplyAsync(() -> discogsWebsiteService.searchArtwork(query.getQuery()));
         }
-        searchSongs(query, songs, fast);
+        final List<TrackMetadataDto> songs = searchSongs(query, fast);
 
         ofNullable(artworkFuture)
                 .flatMap(CompletableFuture::join)
@@ -70,17 +75,20 @@ public class SearchServiceFacadeImpl implements SearchServiceFacade {
         return reverse(songs);
     }
 
-    private void searchSongs(SearchQueryDto query, List<TrackMetadataDto> songs, boolean fast) {
-        for (SearchService searchService : searchServices) {
+    private List<TrackMetadataDto> searchSongs(SearchQueryDto query, boolean fast) {
+        CompletionService<SearchResultDto> completionService = new ExecutorCompletionService<>(searchExecutorService);
+        searchServices.forEach(service -> completionService.submit(() -> ((fast)
+                ? service.searchFast(query)
+                : service.search(query))));
+        List<TrackMetadataDto> songs = newArrayList();
+        for (int i = 0; i < searchServices.size(); i++) {
             try {
-                Function<SearchQueryDto, SearchResultDto> function = fast
-                        ? searchService::searchFast
-                        : searchService::search;
-                songs.addAll(function.apply(query).getSongs());
+                songs.addAll(completionService.take().get().getSongs());
             } catch (Exception e) {
-                log.error(e.getMessage(), e);
+                log.error("Can't take the search service result", e);
             }
         }
+        return songs;
     }
 
     @Override
